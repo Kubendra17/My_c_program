4b30e635433c029426bff25d50cb6c872f9f15b4
Kubendra17 <kubendrakubi36@gmail.com>
2025-05-13 15:45:01 +0530

    Create add2 number by function call.c

---
+++ add2 number by function.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int shridevi(){
+    
+    printf("kubi");
+    printf("date od birth");
+    printf("age");
+    printf("mobilenumber");
+    
+}
+int main()
+{
+    shridevi();
+    return 0;
+}
